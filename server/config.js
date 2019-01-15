5aa02b97302764d0552cff59ad01130f9e278661
Luguitoo <gomezfrancisco.mac@gmail.com>
2019-01-14 20:08:01 -0500

    config update

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-if(process.env.NODE_ENV === 'dev'){
+if(process.env.NODE_ENV !== 'production'){
     urlDB = 'mongodb://localhost:27017/page-cars';
 }else{
     urlDB = 'mongodb://page-cars:a123456@ds011258.mlab.com:11258/page-cars';
